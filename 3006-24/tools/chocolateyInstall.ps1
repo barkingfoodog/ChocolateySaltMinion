1b65164fc8a23c668a8fe7b8240c0a6e6583d49c
Alex Clay <alex@suran.com>
2026-04-30 08:06:42 -0400

    Adding 3006-24, 3007-14, and 3008-0rc1

---
+++ 3006-24/tools/chocolateyInstall.ps1
@@ -0,0 +1,59 @@
+﻿$ErrorActionPreference = 'Stop';
+
+# Python 3
+$url64_py3      = 'https://packages.broadcom.com/artifactory/saltproject-generic/windows/3006.24/Salt-Minion-3006.24-Py3-AMD64-Setup.exe'
+$checksum64_py3 = 'f353f30410ec5ae9245b1e035c3631970b37eb0b180066f17fca210997ed6fcc'
+$url_py3        = 'https://packages.broadcom.com/artifactory/saltproject-generic/windows/3006.24/Salt-Minion-3006.24-Py3-x86-Setup.exe'
+$checksum_py3   = 'f353f30410ec5ae9245b1e035c3631970b37eb0b180066f17fca210997ed6fcc'
+
+$packageArgs = @{
+  packageName     = 'salt-minion'
+  unzipLocation   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  fileType        = 'EXE'
+
+  softwareName    = 'Salt*'
+
+  url             = $url_py3
+  checksum        = $checksum_py3
+  checksumType    = 'sha256'
+
+  url64bit        = $url64_py3
+  checksum64      = $checksum64_py3
+  checksumType64  = 'sha256'
+
+  silentArgs    = "/S"
+  validExitCodes= @(0, 3010, 1641)
+}
+
+
+$packageParameters = Get-PackageParameters
+
+if ($packageParameters['Master']) {
+  $Master = $packageParameters['Master']
+  $packageArgs['silentArgs'] += " /master=$Master"
+}
+
+if ($packageParameters['MinionName']) {
+  $MinionName = $packageParameters['MinionName']
+  $packageArgs['silentArgs'] += " /minion-name=$MinionName"
+}
+
+if ($packageParameters['MinionStart']) {
+  $MinionStart = $packageParameters['MinionStart']
+  $packageArgs['silentArgs'] += " /start-minion=$MinionStart"
+}
+
+if ($packageParameters['MinionStartDelayed'] -eq 'true') {
+  $packageArgs['silentArgs'] += " /start-minion-delayed"
+}
+
+if ($packageParameters['DefaultConfig'] -eq 'true') {
+  $packageArgs['silentArgs'] += " /default-config"
+}
+
+if ($packageParameters['CustomConfig']) {
+  $CustomConfig = $packageParameters['CustomConfig']
+  $packageArgs['silentArgs'] += " /custom-config=$CustomConfig"
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ 3007-14/tools/chocolateyInstall.ps1
@@ -0,0 +1,59 @@
+﻿$ErrorActionPreference = 'Stop';
+
+# Python 3
+$url64_py3      = 'https://packages.broadcom.com/artifactory/saltproject-generic/windows/3007.14/Salt-Minion-3007.14-Py3-AMD64-Setup.exe'
+$checksum64_py3 = 'bc198d5f1ea9c2a4dab1ada261faf3124849afeb06d4d6fa0e1d901d2ebd5a56'
+$url_py3        = 'https://packages.broadcom.com/artifactory/saltproject-generic/windows/3007.14/Salt-Minion-3007.14-Py3-x86-Setup.exe'
+$checksum_py3   = '9ed34f4a5f1603f5617ee4d72b27c7169b902ed6c6a6d7ba2e5036c96ee248eb'
+
+$packageArgs = @{
+  packageName     = 'salt-minion'
+  unzipLocation   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  fileType        = 'EXE'
+
+  softwareName    = 'Salt*'
+
+  url             = $url_py3
+  checksum        = $checksum_py3
+  checksumType    = 'sha256'
+
+  url64bit        = $url64_py3
+  checksum64      = $checksum64_py3
+  checksumType64  = 'sha256'
+
+  silentArgs    = "/S"
+  validExitCodes= @(0, 3010, 1641)
+}
+
+
+$packageParameters = Get-PackageParameters
+
+if ($packageParameters['Master']) {
+  $Master = $packageParameters['Master']
+  $packageArgs['silentArgs'] += " /master=$Master"
+}
+
+if ($packageParameters['MinionName']) {
+  $MinionName = $packageParameters['MinionName']
+  $packageArgs['silentArgs'] += " /minion-name=$MinionName"
+}
+
+if ($packageParameters['MinionStart']) {
+  $MinionStart = $packageParameters['MinionStart']
+  $packageArgs['silentArgs'] += " /start-minion=$MinionStart"
+}
+
+if ($packageParameters['MinionStartDelayed'] -eq 'true') {
+  $packageArgs['silentArgs'] += " /start-minion-delayed"
+}
+
+if ($packageParameters['DefaultConfig'] -eq 'true') {
+  $packageArgs['silentArgs'] += " /default-config"
+}
+
+if ($packageParameters['CustomConfig']) {
+  $CustomConfig = $packageParameters['CustomConfig']
+  $packageArgs['silentArgs'] += " /custom-config=$CustomConfig"
+}
+
+Install-ChocolateyPackage @packageArgs
